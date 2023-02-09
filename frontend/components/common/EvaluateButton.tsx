d44249387371d0a1bc393141e5958daa739eb86f
Inkyu Kim <inkyu0103@gmail.com>
2023-02-09 14:44:06 +0900

    feat: add chart layout

--- frontend/components/common/EvaluateButton.tsx
+++ frontend/components/common/EvaluateButton.tsx
@@ -1,5 +1,5 @@
 const EvaluateButton = () => (
-  <button className="w-full h-8 bg-blue-300 rounded-md hover:cursor-pointer">
+  <button className="w-full h-8 bg-blue-300 rounded-md hover:cursor-pointer max-w-[328px]">
     평가하기
   </button>
 );
--- frontend/components/rackets/RacketDetail.tsx
+++ frontend/components/rackets/RacketDetail.tsx
@@ -1,19 +1,22 @@
 import EvaluateButton from "components/common/EvaluateButton";
 import { RacketDetailProps } from "interface/RacketDetail.interface";
+import Chart from "components/rackets/Chart";
 
 const RacketDetail = ({ racketName }: RacketDetailProps) => (
-  <div className="px-4">
+  <div className="px-4 max-w-[1200px] mx-auto border-2">
     <h1 className="my-10 text-3xl font-bold">{racketName}</h1>
-    <div className="container justify-between mx-auto md:flex mx-w-[1200px]">
-      <div className="">
+    <div className="justify-between mx-auto md:flex border-2 border-orange-100">
+      <div className="md:w-1/2 my-4">
         <img
           alt="racket"
           src="https://m.woosungsports.com/web/product/big/412_5ff22e3f894ac8106c2773bec3fbe12c.jpg"
           className="w-[328px] mx-auto border-2 "
         />
       </div>
-      <div className="md:w-1/2">
-        <div>그래프</div>
+      <div className="w-[328px] mx-auto md:w-1/2 md:flex md:flex-col md:items-center  ">
+        <div className="w-[328px] h-[328px] border-2 my-4 flex items-center">
+          <Chart />
+        </div>
         <EvaluateButton />
       </div>
     </div>
