dd2d3eaf7694683dbf3c821d4b16a8d285acb5c8
Inkyu Kim <inkyu0103@gmail.com>
2023-02-09 17:23:35 +0900

    chore: remove unused border

--- frontend/components/rackets/RacketDetail.tsx
+++ frontend/components/rackets/RacketDetail.tsx
@@ -1,11 +1,43 @@
 import EvaluateButton from "components/common/EvaluateButton";
 import { RacketDetailProps } from "interface/RacketDetail.interface";
 import Chart from "components/rackets/Chart";
+import Review from "components/rackets/Review";
+
+const testReivew = [
+  {
+    createdAt: "1시간 전",
+    title: "와하하",
+    userId: "test-user",
+    content: "이 라켓은 정말 최고입니다. 와하하 정말 후회하지 않으십니다요",
+    value: 5,
+  },
+  {
+    createdAt: "2시간 전",
+    title: "와하하",
+    userId: "test-user",
+    content: "이 라켓은 정말 최고입니다. 와하하 정말 후회하지 않으십니다요",
+    value: 5,
+  },
+  {
+    createdAt: "3시간 전",
+    title: "와하하",
+    userId: "test-user",
+    content: "이 라켓은 정말 최고입니다. 와하하 정말 후회하지 않으십니다요",
+    value: 5,
+  },
+  {
+    createdAt: "4시간 전",
+    title: "와하하",
+    userId: "test-user",
+    content: "이 라켓은 정말 최고입니다. 와하하 정말 후회하지 않으십니다요",
+    value: 5,
+  },
+];
 
 const RacketDetail = ({ racketName }: RacketDetailProps) => (
-  <div className="px-4 max-w-[1200px] mx-auto border-2">
+  <div className="px-4 max-w-[1200px] mx-auto mb-9">
     <h1 className="my-10 text-3xl font-bold">{racketName}</h1>
-    <div className="justify-between mx-auto md:flex border-2 border-orange-100">
+    <div className="justify-between mx-auto md:flex ">
       <section className="md:w-1/2 my-4">
         <img
           alt="racket"
@@ -20,8 +52,13 @@ const RacketDetail = ({ racketName }: RacketDetailProps) => (
         <EvaluateButton />
       </section>
     </div>
-    <section className=" border-2 border-orange-300 mx-auto">
-      <h1 className="my-10 text-1xl font-bold">리뷰</h1>
+    <section className="mx-auto">
+      <p className="my-4 text-2xl font-bold">리뷰</p>
+      <div className="flex flex-col gap-y-2">
+        {testReivew.map((ele, idx) => (
+          <Review key={idx} {...ele} />
+        ))}
+      </div>
     </section>
   </div>
 );
--- frontend/components/rackets/Review.tsx
+++ frontend/components/rackets/Review.tsx
@@ -2,7 +2,7 @@ import { ReviewProps } from "interface/Review.interface";
 import ReactStars from "react-rating-stars-component";
 
 const Review = ({ userId, content, value, createdAt, title }: ReviewProps) => (
-  <article className="w-full border-2 rounded-md drop-shadow-sm p-2 hover:cursor-pointer hover:bg-slate-100 duration-300 ease-out ">
+  <article className="w-full border-2 rounded-md drop-shadow-sm p-2 hover:cursor-pointer hover:bg-slate-100 duration-300 ease-out">
     <div className="flex justify-between">
       <ReactStars edit={false} value={value} />
       <p className="text-xs">{createdAt}</p>
