c1dc41e5119bb3dd8e4c8e9d8c4d1f0a01e68e50
Inkyu Kim <inkyu0103@gmail.com>
2023-02-15 17:04:31 +0900

    chore: test fetching data

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,33 @@
 import type { NextPage } from "next";
 import Racket from "components/rackets/Racket";
 import Header from "components/common/Header";
+import { useEffect, useState } from "react";
 
-const Home: NextPage = () => (
-  <div>
-    <Header />
-    <div className="mx-auto my-10 max-w-[1200px] flex flex-wrap">
-      {data.map((name, idx) => (
-        <Racket key={idx} name={name} />
-      ))}
+const Home: NextPage = () => {
+  const [t, setT] = useState(null);
+
+  useEffect(() => {
+    console.log("히히");
+    fetch("http://localhost:3000")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setT(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div>{t?.result}</div>
+      <div className="mx-auto my-10 max-w-[1200px] flex flex-wrap">
+        {data.map((name, idx) => (
+          <Racket key={idx} name={name} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
 
