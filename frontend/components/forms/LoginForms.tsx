42812cfc63cb5bc0efe0bd42d7b2948069c88e9e
Inkyu Kim <inkyu0103@gmail.com>
2023-02-18 23:54:16 +0900

    feat: add login page (#31)

--- frontend/components/forms/LoginForms.tsx
+++ frontend/components/forms/LoginForms.tsx
@@ -2,11 +2,26 @@ import Input from "components/forms/Input";
 
 const LoginForms = () => {
   return (
-    <form className="flex flex-col gap-2">
-      <Input type="email" placeholder="이메일을 입력해주세요" />
-      <Input type="password" placeholder="비밀번호를 입력해주세요" />
-      <button onClick={()=>alert('로그인 버튼')}>로그인</button>
-    </form>
+    <div className="flex flex-col items-center justify-center w-screen h-full ">
+      <div>로고</div>
+      <form
+        className="w-[328px] flex flex-col gap-2 "
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <Input type="email" placeholder="이메일을 입력해주세요" isRequired />
+        <Input
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          isRequired
+        />
+        <button className="w-full h-8 bg-blue-300 rounded-md hover:cursor-pointer hover:text-white max-w-[328px] duration-300 ease-out hover:bg-blue-500">
+          로그인
+        </button>
+        <button className="w-full h-8 bg-blue-100 rounded-md hover:cursor-pointer hover:text-white max-w-[328px] duration-300 ease-out hover:bg-blue-300">
+          회원가입
+        </button>
+      </form>
+    </div>
   );
 };
 export default LoginForms;
--- frontend/pages/pages.tsx
+++
@@ -1,5 +0,0 @@
-const Pages = () => {
-  return <div>안녕하세요 여기는 페이지입니다.</div>;
-};
-
-export default Pages;
