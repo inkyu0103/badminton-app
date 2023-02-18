79ee0d361cb5a66f911dea647f35251797c28711
Inkyu Kim <inkyu0103@gmail.com>
2023-02-18 23:42:45 +0900

    feat: add login page

--- frontend/components/forms/Input.tsx
+++ frontend/components/forms/Input.tsx
@@ -1,15 +1,16 @@
+import { InputProps } from "interface/Input.interface";
+
 const Input = ({
   type = "text",
   placeholder,
-}: {
-  type: string;
-  placeholder?: string;
-}) => {
+  isRequired = false,
+}: InputProps) => {
   return (
     <input
       placeholder={placeholder}
       type={type}
-      className="py-2 outline outline-1 outline-blue-100 rounded-md hover:outline-blue-200 text-sm indent-2"
+      className="py-2 text-sm rounded-md outline outline-1 outline-blue-100 hover:outline-blue-200 indent-2"
+      required={isRequired}
     />
   );
 };
---
+++ frontend/pages/login.tsx
@@ -0,0 +1,10 @@
+import Header from "components/common/Header";
+import LoginForms from "components/forms/LoginForms";
+
+const Login = () => (
+  <div className="h-screen">
+    <Header />
+    <LoginForms />
+  </div>
+);
+export default Login;
