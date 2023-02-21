edede88efd11a075f18784490d0a9954c58c4a69
Inkyu Kim <inkyu0103@gmail.com>
2023-02-21 22:16:35 +0900

    feat: add lottie component

---
+++ frontend/components/common/EmailSend.stories.tsx
@@ -0,0 +1,7 @@
+import EmailSend from "components/common/EmailSend";
+
+export default {
+  component: EmailSend,
+};
+
+export const Default = () => <EmailSend />;
---
+++ frontend/components/common/EmailSend.tsx
@@ -0,0 +1,8 @@
+import Lottie from "lottie-react";
+import emailSend from "assets/lottie/emailSend.json";
+const EmailSend = () => (
+  <div className="w-[360px]">
+    <Lottie animationData={emailSend} loop={false} />
+  </div>
+);
+export default EmailSend;
