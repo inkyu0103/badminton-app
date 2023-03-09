da69faec9c34b5c3d04125064c5f35a426be92e4
Inkyu Kim <inkyu0103@gmail.com>
2023-03-09 14:14:41 +0900

    feat: add swagger config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mobae')
+    .setDescription('Mobae API')
+    .setVersion('1.0')
+    .addTag('mobae')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
   app.useGlobalPipes(new ValidationPipe());
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
