051bbd876eb4e450d056a81532043870f5e0f5de
Inkyu Kim <inkyu0103@gmail.com>
2023-03-12 00:23:01 +0900

    chore: recoil setting

--- frontend/components/common/Header.tsx
+++ frontend/components/common/Header.tsx
@@ -1,10 +1,38 @@
 import Link from "next/link";
+import { useRecoilState } from "recoil";
+import { loginState } from "recoil/atoms/loginState";
 
-const Header = () => (
-  <header className="flex items-center justify-end h-12 px-4 bg-black w-vw">
-    <Link href="/login" className="text-white">
-      로그인
-    </Link>
-  </header>
-);
+const Header = () => {
+  const [loginUser, setLoginUser] = useRecoilState(loginState);
+
+  return (
+    <header className="flex items-center justify-end h-12 px-4 bg-black w-vw gap-x-2">
+      {loginUser ? (
+        <>
+          <Link href="/rackets/yonex" className="text-white">
+            Rackets
+          </Link>
+          <Link href="/emailVerify" className="text-white">
+            프로필
+          </Link>
+          <button className="text-white" onClick={() => setLoginUser(null)}>
+            로그아웃
+          </button>
+        </>
+      ) : (
+        <>
+          <Link href="/rackets/yonex" className="text-white">
+            Rackets
+          </Link>
+          <Link href="/emailVerify" className="text-white">
+            회원가입
+          </Link>
+          <Link href="/login" className="text-white">
+            로그인
+          </Link>
+        </>
+      )}
+    </header>
+  );
+};
 export default Header;
