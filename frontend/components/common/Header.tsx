3cd69d866eb2f2c05f1283c9ccc3f497afa4e5ac
Inkyu Kim <inkyu0103@gmail.com>
2023-03-12 00:44:18 +0900

    refactor: seperate header view

--- frontend/components/common/Header.tsx
+++ frontend/components/common/Header.tsx
@@ -5,34 +5,38 @@ import { loginState } from "recoil/atoms/loginState";
 const Header = () => {
   const [loginUser, setLoginUser] = useRecoilState(loginState);
 
-  return (
-    <header className="flex items-center justify-end h-12 px-4 bg-black w-vw gap-x-2">
-      {loginUser ? (
-        <>
-          <Link href="/rackets/yonex" className="text-white">
-            Rackets
-          </Link>
-          <Link href="/emailVerify" className="text-white">
-            프로필
-          </Link>
-          <button className="text-white" onClick={() => setLoginUser(null)}>
-            로그아웃
-          </button>
-        </>
-      ) : (
-        <>
-          <Link href="/rackets/yonex" className="text-white">
-            Rackets
-          </Link>
-          <Link href="/emailVerify" className="text-white">
-            회원가입
-          </Link>
-          <Link href="/login" className="text-white">
-            로그인
-          </Link>
-        </>
-      )}
-    </header>
-  );
+  const handleLogout = () => {};
+
+  return <HeaderView handleLogout={handleLogout} loginUser={loginUser} />;
 };
 export default Header;
+
+export const HeaderView = ({ handleLogout, loginUser }) => (
+  <header className="flex items-center justify-end h-12 px-4 bg-black w-vw gap-x-2">
+    {loginUser ? (
+      <>
+        <Link href="/rackets/yonex" className="text-white">
+          Rackets
+        </Link>
+        <Link href="/emailVerify" className="text-white">
+          프로필
+        </Link>
+        <button className="text-white" onClick={handleLogout}>
+          로그아웃
+        </button>
+      </>
+    ) : (
+      <>
+        <Link href="/rackets/yonex" className="text-white">
+          Rackets
+        </Link>
+        <Link href="/emailVerify" className="text-white">
+          회원가입
+        </Link>
+        <Link href="/login" className="text-white">
+          로그인
+        </Link>
+      </>
+    )}
+  </header>
+);
