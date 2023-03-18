588a77a0c69c2be48fb0b4d33f227120d9bd653f
Inkyu Kim <inkyu0103@gmail.com>
2023-03-18 22:42:29 +0900

    feat: add get racket api (backend)

--- backend/src/rackets/rackets.controller.ts
+++ backend/src/rackets/rackets.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { PagenationPipe } from './pagination.pipe';
+import { RacketsService } from './rackets.service';
 
 @Controller('rackets')
-export class RacketsController {}
+export class RacketsController {
+  constructor(private readonly racketsService: RacketsService) {}
+
+  @Get('/:brand')
+  async getRacketList(
+    @Param('brand') brand: string,
+    @Query('page', PagenationPipe) page: number,
+  ) {
+    return this.racketsService.getRacketList(brand, page);
+  }
+}
