d37473d0fcf3e0de763637b009af187f4216437c
Inkyu Kim <inkyu0103@gmail.com>
2023-03-27 23:55:22 +0900

    refactor: use dynamic module

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,4 +1,11 @@
-import SignupForms from "components/forms/SignupForms";
+import React from "react";
+import Spinner from "components/common/Spinner";
+import dynamic from "next/dynamic";
+
+const SignupForms = dynamic(() => import("components/forms/SignupForms"), {
+  loading: () => <Spinner />,
+  ssr: false,
+});
 
 const Signup = () => <SignupForms />;
 export default Signup;
