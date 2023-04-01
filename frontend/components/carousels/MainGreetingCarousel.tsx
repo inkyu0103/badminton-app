075a0457d585270c400c2931aee395d451fecf83
Inkyu Kim <inkyu0103@gmail.com>
2023-04-02 01:34:27 +0900

    chore

--- frontend/components/carousels/MainGreetingCarousel.tsx
+++ frontend/components/carousels/MainGreetingCarousel.tsx
@@ -2,7 +2,7 @@ import Slider from "react-slick";
 import EquipmentGuide from "components/carousels/contents/EquipmentGuide";
 
 const MainGreetingCarousel = () => (
-  <section>
+  <section className="test">
     <p className="text-2xl font-bold">안녕하세요 :)</p>
     <Slider arrows={false} slidesToShow={1} className="flex gap-x-4">
       <EquipmentGuide />
