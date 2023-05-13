85597007e7cbbbfd903f19a450526550cb1d3513
Inkyu Kim <inkyu0103@gmail.com>
2023-05-14 01:59:00 +0900

    chore: add Icons

---
+++ frontend/components/icons/GithubIcon.stories.tsx
@@ -0,0 +1,7 @@
+import GithubIcon from "components/icons/GithubIcon";
+
+export default {
+  component: GithubIcon,
+};
+
+export const Default = () => <GithubIcon />;
---
+++ frontend/components/icons/Mailicon.stories.tsx
@@ -0,0 +1,7 @@
+import MailIcon from "components/icons/MailIcon";
+
+export default{
+  component: MailIcon
+}
+
+export const Default = () => <MailIcon />;
