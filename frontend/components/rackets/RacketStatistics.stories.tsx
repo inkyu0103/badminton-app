69da1b707b304a00c6e78ebd60c6f35e761478a9
Inkyu Kim <inkyu0103@gmail.com>
2023-05-15 17:28:29 +0900

    fix: fix storybook error

--- frontend/components/rackets/RacketStatistics.stories.tsx
+++ frontend/components/rackets/RacketStatistics.stories.tsx
@@ -1,7 +1,22 @@
 import { RacketStatisticsView } from "components/rackets/RacketStatistics";
+import { IRacketStatisticsProps } from "interface/Statistics.interface";
 
 export default {
   component: RacketStatisticsView,
+  parameters: {
+    chromatic: { disableSnapshot: true },
+  },
 };
 
-export const Default = () => <RacketStatisticsView />;
+export const Default = (args: IRacketStatisticsProps) => (
+  <RacketStatisticsView {...args} />
+);
+
+Default.args = {
+  reviewStatistics: undefined,
+  handleChangeRank: () => {},
+};
+
+Default.argTypes = {
+  handleChangeRank: { table: { disable: true } },
+};
