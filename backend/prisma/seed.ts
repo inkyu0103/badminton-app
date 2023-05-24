6073dd7fbde08c5b8f077e7ec4a6bf53a4f54f9b
Inkyu Kim <inkyu0103@gmail.com>
2023-05-24 12:43:28 +0900

    refactor: refactor seed script

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,26 +1,42 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Racket } from '@prisma/client';
 
 const client = new PrismaClient();
 
-const testRacket = () =>
-  [...Array(20).keys()].map(
-    async (idx, _) =>
-      await client.racket.create({
-        data: {
-          id: idx + 10,
-          name: `나노레이 ${idx + 1}`,
-          image: null,
-          price: 10000,
-          shaft: 'FLEXIBLE',
-          weight: 'W3U',
-          score: 5,
-          brandId: 1,
-        },
-      }),
-  );
+const testRackets: Racket[] = new Array(20).fill(0).map((_, idx) => ({
+  id: idx + 10,
+  name: `나노레이 ${idx + 1}`,
+  image: null,
+  price: 10000,
+  shaft: 'FLEXIBLE',
+  weight: 'W3U',
+  score: 0,
+  brandId: 1,
+}));
+
+const makeTestBrand = () => {
+  return client.brand.create({
+    data: {
+      id: 1,
+      name: 'yonex',
+    },
+  });
+};
+
+const makeTestRackets = () => {
+  return client.racket.createMany({
+    data: testRackets,
+  });
+};
 
 const main = async () => {
-  Promise.all(testRacket());
+  console.time('⏰ total elapsed time');
+  console.log('💾 Setup test data...');
+  await makeTestBrand();
+  console.log('✅ complete makeTestBrand');
+  await makeTestRackets();
+  console.log('✅ complete makeTestRackets');
+
+  console.timeEnd('⏰ total elapsed time');
 };
 
 main()
