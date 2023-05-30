db559ae2d34bab39d572bcfad9971ed1b6cf93c7
Inkyu Kim <inkyu0103@gmail.com>
2023-05-30 11:28:36 +0900

    feat: add user & review seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,6 +1,9 @@
-import { PrismaClient, Racket } from '@prisma/client';
+import { PrismaClient, Racket, RacketReview, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const client = new PrismaClient();
+
+const testRanks = ['S', 'A', 'B', 'C', 'D', 'E'] as const;
 
 const testRackets: Racket[] = new Array(20).fill(0).map((_, idx) => ({
   id: idx + 10,
@@ -11,6 +14,31 @@ const testRackets: Racket[] = new Array(20).fill(0).map((_, idx) => ({
   weight: 'W3U',
   score: 0,
   brandId: 1,
+}));
+
+const testUsers: User[] = new Array(20).fill(0).map((_, idx) => ({
+  id: idx,
+  nickname: `testUser ${idx}`,
+  email: `testUser${idx}@test.com`,
+  password: bcrypt.hashSync('testpassword', 10),
+  birthday: new Date('1995-01-01'),
+  gender: idx % 2 ? 'MALE' : 'FEMALE',
+  rank: testRanks[idx % 6],
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}));
+
+const testReviews: RacketReview[] = new Array(20).fill(0).map((_, idx) => ({
+  id: idx + 20,
+  control: Math.round(Math.random()),
+  power: Math.round(Math.random()),
+  weight: Math.floor(Math.random() * 3),
+  racketId: 11,
+  review: `${idx} 번 째 테스트 리뷰입니다. 라켓에 대한 정보는 따로 없습니다.`,
+  userId: idx,
+  starRating: Math.floor(Math.random() * 6),
+  createdAt: new Date(),
+  updatedAt: new Date(),
 }));
 
 const makeTestBrand = () => {
@@ -28,6 +56,18 @@ const makeTestRackets = () => {
   });
 };
 
+const makeTestUsers = () => {
+  return client.user.createMany({
+    data: testUsers,
+  });
+};
+
+const makeTestReviews = () => {
+  return client.racketReview.createMany({
+    data: testReviews,
+  });
+};
+
 const main = async () => {
   console.time('⏰ total elapsed time');
   console.log('💾 Setup test data...');
@@ -35,6 +75,10 @@ const main = async () => {
   console.log('✅ complete makeTestBrand');
   await makeTestRackets();
   console.log('✅ complete makeTestRackets');
+  await makeTestUsers();
+  console.log('✅ complete makeTestUsers');
+  await makeTestReviews();
+  console.log('✅ complete makeTestReviews');
 
   console.timeEnd('⏰ total elapsed time');
 };
