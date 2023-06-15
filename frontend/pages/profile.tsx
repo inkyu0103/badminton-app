897fb686c958697effc97c8d1b86014d060710ae
Inkyu Kim <inkyu0103@gmail.com>
2023-06-15 20:42:09 +0900

    refactor: use NextSeo with each page

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,13 +1,20 @@
 import ProfileSkeleton from "components/users/ProfileSkeleton";
+import seoConfig from "constants/seoConfig";
 import { GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
+import { NextSeo } from "next-seo";
 
 const Profile = dynamic(() => import("components/users/Profile"), {
   loading: () => <ProfileSkeleton />,
   ssr: false,
 });
 
-const ProfilePage = () => <Profile />;
+const ProfilePage = () => (
+  <>
+    <NextSeo title={seoConfig.profile.tilte} />
+    <Profile />
+  </>
+);
 
 export default ProfilePage;
 
