2a823804a30c1c2df5651fa8e259cb89ed14c557
Inkyu Kim <inkyu0103@gmail.com>
2023-06-19 23:22:31 +0900

    temp: temp

--- frontend/__tests__/Login.test.tsx
+++ frontend/__tests__/Login.test.tsx
@@ -1,8 +1,6 @@
 import { render } from "@testing-library/react";
 import { useRouter } from "next/router";
 import Login from "pages/login";
-import { RecoilRoot } from "recoil";
-import { userState } from "recoil/atoms/user";
 
 jest.mock("next/router", () => ({
   useRouter: jest.fn(),
@@ -17,11 +15,7 @@ jest.mock("@tanstack/react-query", () => ({
 
 describe("로그인 페이지 접근 가능 여부를 테스트 합니다.", () => {
   it("로그인 되지 않은 경우, 접근 가능합니다.", () => {
-    render(
-      <RecoilRoot initializeState={({ set }) => set(userState, null)}>
-        <Login />
-      </RecoilRoot>,
-    );
+    render(<Login />);
   });
 
   it("로그인 된 경우, 접근이 불가능합니다.", () => {
@@ -30,16 +24,8 @@ describe("로그인 페이지 접근 가능 여부를 테스트 합니다.", () 
       push: pushMock,
     });
 
-    render(
-      <RecoilRoot
-        initializeState={({ set }) =>
-          set(userState, { email: "test@test.com", id: 1 })
-        }
-      >
-        <Login />
-      </RecoilRoot>,
-    );
+    render(<Login />);
 
-    expect(useRouter().push).toBeCalledWith("/");
+    expect(useRouter().push).toBeCalledWith("/login");
   });
 });
