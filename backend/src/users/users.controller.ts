3ae51b12a61ddc8ba19410821f4904c91e551d54
inkyu0103 <inkyu0103@gmail.com>
2023-08-30 16:03:37 +0900

    feat: add query user's club

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { UsersService } from 'users/users.service';
 
 @Controller('users')
@@ -8,5 +8,10 @@ export class UsersController {
   @Get('/usable-nickname')
   async isUsableNickname(@Query('nickname') nickname: string) {
     return await this.usersService.isUsableNickname(nickname);
+  }
+
+  @Get('/:userId/clubs')
+  async getUserClubs(@Param('userId', ParseIntPipe) userId: number) {
+    return await this.usersService.getUserClubs(userId);
   }
 }
